8052a7c443d4476929195f8c35d2cdc76e2f9dd4
Lana <112628962+svetlanatad@users.noreply.github.com>
2024-04-25 14:56:49 +0400

    aadded fix

--- Assets/Scripts/lose.cs
+++ Assets/Scripts/lose.cs
@@ -8,18 +8,25 @@ public class Lose : MonoBehaviour
 {
     public TextMeshProUGUI presse;
     public TextMeshProUGUI lose;
+    public TextMeshProUGUI interactionTextUI;
     public Button loseButton;
+    public bool active = false;
    
     private void OnTriggerEnter2D(Collider2D collision)
     {
-        if (collision.gameObject.tag.Equals("Enemy"))
+        if (collision.gameObject.tag.Equals("Enemy") && !active)
         {
-            lose.gameObject.SetActive(true);
+            lose.gameObject.SetActive(active);
             loseButton.gameObject.SetActive(true);
 
             presse.gameObject.SetActive(false);
             Debug.Log("Enemy");
         }
+
+	
+        presse.gameObject.SetActive(false);
+interactionTextUI.gameObject.SetActive(false);
+        
         Debug.Log("Lost");
     }
 }
--- Assets/Scripts/win.cs
+++ Assets/Scripts/win.cs
@@ -8,16 +8,18 @@ public class Win : MonoBehaviour
 {
     public TextMeshProUGUI interactionTextUI; // Reference to the TextMeshProUGUI element displaying interaction prompt
     public TextMeshProUGUI presse;
-    
+    public bool active = false; 
    
     private void OnTriggerEnter2D(Collider2D collision)
     {
-        if (collision.gameObject.tag.Equals("Player"))
+        if (collision.gameObject.tag.Equals("Player") && !active)
         {
-            interactionTextUI.gameObject.SetActive(true);
+            interactionTextUI.gameObject.SetActive(active);
             Debug.Log("Won");
             presse.gameObject.SetActive(false);
         }
+	
+            presse.gameObject.SetActive(false);
         Debug.Log("aaa");
 
     }
